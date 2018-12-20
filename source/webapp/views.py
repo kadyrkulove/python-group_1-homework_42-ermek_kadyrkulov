2b230e5ca57e02643f1f1519aeb3b7efdefa3f9d
Ermek Kadyrkulov <kadyrkulove@gmail.com>
2018-12-20 17:10:06 +0600

    add templates html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from webapp.models import User, Article, Comment, Rating
 
-# Create your views here.
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class UserFavoritesView(DetailView):
+    model = User
+    template_name = 'user_favorites.html'
